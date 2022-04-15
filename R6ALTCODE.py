604498ffe9f7c9dba18cd6d62e17b83da5fbdb3d
ArnaudCabello <petitarnoj@gmail.com>
2022-04-15 12:58:43 -0400

    v2

--- R6ALTCODE.py
+++ R6ALTCODE.py
@@ -1,34 +1,11 @@
-from asyncio.windows_events import NULL
-from cgitb import text
-from distutils.command.config import config
-from pickle import EMPTY_LIST
+from distutils import command
 import subprocess
-from tkinter import filedialog
 import pyautogui, time
 from tkinter import *
-from tkinter import filedialog as fd
-import os
 
 root = Tk()
 root.title('R6')
 root.geometry("600x400")
-
-def save(listPath):
-    f = open("R6ALT PATHS", "w")
-    f.truncate(0);
-    f.seek(0)
-    i = 0
-    while (i < 3):
-        f.write(listPath[i])
-        f.write('\n')
-        i+=1
-    f.close()
-
-def uploadFile(listPath):
-    file = fd.askopenfilename()
-    listPath.append(file)
-    print(listPath)
-    
 
 def getChoices(lines,count):
     display = []
@@ -38,7 +15,7 @@ def getChoices(lines,count):
         i+=4
     return display
 
-def selection(lines,count,ubiPath,iniPath):
+def selection(lines,count):
     i = 0;
     accinfo =[]
     while(i < count):
@@ -49,16 +26,18 @@ def selection(lines,count,ubiPath,iniPath):
             break
         i += 1
 
+
     gameSettings = []
 
     dataCenter = "DataCenterHint=playfab/" + serverVar.get() + "\n"
+
+    start = "C:\\Users\\Arnaud Cabello\\OneDrive\\Documents\\My Games\\Rainbow Six - Siege\\"
 
     end = "\\GameSettings.ini"
 
     subString = accinfo[2][0:36]
 
-    temp = iniPath + subString + end
-    print(temp)
+    temp = start + subString + end
     textFile = open(temp, "r+")
     gameSettings = textFile.readlines()
     count = 0
@@ -70,12 +49,11 @@ def selection(lines,count,ubiPath,iniPath):
     while(i < count):
         if(len(gameSettings[i]) > 13):
             if(gameSettings[i][0:15] == "DataCenterHint="):
-                print("in the loop")
                 gameSettings[i] = dataCenter
         textFile.write(gameSettings[i])
         i+=1
     textFile.close()
-    subprocess.Popen([ubiPath])
+    subprocess.Popen([r'C:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\UbisoftConnect.exe'])
     time.sleep(10)
     pyautogui.write(accinfo[0]) 
     pyautogui.moveTo(1000, 550) 
@@ -85,73 +63,31 @@ def selection(lines,count,ubiPath,iniPath):
     pyautogui.click()
     return accinfo
 
-configFile = os.getcwd()
-configFile = configFile + "\R6ALT PATHS.txt"
-print(configFile)
 
-configLines = []
-with open(configFile,"r") as f:
-    configLines = f.readlines()
+lines = []
+with open(r'C:\Users\Arnaud Cabello\OneDrive\Desktop\R6 ALT\username.txt') as f:
+    lines = f.readlines()
     count = 0
-    for configLine in configLines:
+    for line in lines:
          count += 1
-if len(configLines) > 0:
 
-    ubiPath = configLines[0]
-    ubiPath = ubiPath[0:(len(ubiPath)-1)]
-    userInfo = configLines[1]
-    userInfo = userInfo[0:(len(userInfo)-1)]
-    iniPath = configLines[2]
-    iniPath = iniPath[0:(len(iniPath)-54)]
+choices = getChoices(lines,count)
+variable = StringVar(root)
+variable.set('Select An Account')
 
-    lines = []
-    with open(userInfo,"r") as f:
-        lines = f.readlines()
-        count = 0
-        for line in lines:
-            count += 1
+account = OptionMenu(root, variable, *choices)
 
-    choices = getChoices(lines,count)
-    variable = StringVar(root)
-    variable.set('Select An Account')
+serverOptions = ["eastus" , "westus", "centralus", "southcentralus"]
+serverVar = StringVar(root)
+serverVar.set('Select A Server')
 
-    account = OptionMenu(root, variable, *choices)
+server = OptionMenu(root, serverVar, *serverOptions)
 
-    serverOptions = ["eastus" , "westus", "centralus", "southcentralus"]
-    serverVar = StringVar(root)
-    serverVar.set('Select A Server')
+account.pack();
 
-    server = OptionMenu(root, serverVar, *serverOptions)
-
-    account.pack();
-
-    server.pack();
+server.pack();
 
 
-    button_1 = Button(root,text="Select", padx = 50, pady = 20, command=lambda: selection(lines,count,ubiPath,iniPath))
-    button_1.place(x=250, y = 320)
-else:
-    listPath = []
-    myFont1 = ('times',12,'bold')
-    root.title("Settings")
-    l5 = Label(root, text = 'First Time Setup Configure Files',width=30, font = myFont1)
-    l5.pack(ipadx= 100, ipady = 10)
-    root.geometry("400x400")
-
-    l1 = Label(root, text = 'File path to Ubisoft Connect Launcher',width=30, font = myFont1)
-    l1.pack(ipadx= 100, ipady = 10)
-    b1 = Button(root,text='File path to Ubisoft Connect Launcher', width=30,command=lambda:uploadFile(listPath)).pack()
-
-    l2 = Label(root, text = 'File path to R6ALT info',width=30, font = myFont1)
-    l2.pack(ipadx= 100, ipady = 10)
-    b2 = Button(root,text='File path to R6ALT info', width=30,command=lambda:uploadFile(listPath)).pack()
-
-    l3 = Label(root, text = 'File path to game ini file',width=30, font = myFont1)
-    l3.pack(ipadx= 100, ipady = 10)
-    b3 = Button(root,text='File path to game ini file', width=30,command=lambda:uploadFile(listPath)).pack()
-
-    btnSave = Button(root, text = "Save", command=lambda:save(listPath)).pack()
-    btnClose = Button(root, text = "close window", command=root.destroy).pack()
-        
-
+button_1 = Button(root,text="Select", padx = 50, pady = 20, command=lambda: selection(lines,count))
+button_1.place(x=250, y = 320)
 root.mainloop()
